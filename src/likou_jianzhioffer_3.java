4a6094f04d553482cfede814505eef17b77bbd57
chendongtao <1445676933@qq.com>
2021-05-06 14:44:05 +0800

    20210506-力扣刷题

--- src/likou_jianzhioffer_3.java
+++ src/likou_jianzhioffer_3.java
@@ -6,9 +6,13 @@ import java.util.*;
  * @Description:
  */
 public class likou_jianzhioffer_3 {
+    private static int[] nums, tmp;
+
     public static void main(String[] args) {
-        String str = "aabbc";
-        System.out.println(firstUniqChar(str));
+        /*String str = "aabbc";
+        System.out.println(firstUniqChar(str));*/
+        /*int[] nums = {7, 3, 2, 6, 0, 1, 5, 4};
+        System.out.println(reversePairs(nums));*/
     }
 
     /**
@@ -101,10 +105,64 @@ public class likou_jianzhioffer_3 {
     /**
      * 剑指 Offer 51. 数组中的逆序对
      *
-     * @param nums
+     * @param num
      * @return
      */
-    public int reversePairs(int[] nums) {
-        return -1;
+    public static int reversePairs(int[] num) {
+        if (num.length < 2) {
+            return 0;
+        }
+        nums = num;
+        tmp = new int[num.length];
+        return mergeSort(0, num.length - 1);
+    }
+
+    //归并排序
+    private static int mergeSort(int l, int r) {
+        if (l >= r) {
+            return 0;
+        }
+        int mid = (l + r) / 2;
+        int res = mergeSort(l, mid) + mergeSort(mid + 1, r);
+        for (int k = l; k <= r; k++) {
+            tmp[k] = nums[k];
+        }
+        //合并阶段
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i == mid + 1) {
+                nums[k] = tmp[j++];
+            } else if (j == r + 1 || tmp[i] <= tmp[j]) {
+                nums[k] = tmp[i++];
+            } else {
+                nums[k] = tmp[j++];
+                res += mid - i + 1;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 剑指 Offer 52. 两个链表的第一个公共节点
+     *
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode sameNode = null;
+        if (headA == null || headB == null) {
+            return null;
+        }
+        if (headA.next == headB.next) {
+            sameNode = headA.next;
+            return sameNode;
+        } else {
+            sameNode = getIntersectionNode(headA.next, headB);
+            if (sameNode == null) {
+                sameNode = getIntersectionNode(headA, headB.next);
+            }
+        }
+        return sameNode;
     }
 }
