98d8e40e6b029428cce1bac565d7cfde9956bbe3
chendongtao <1445676933@qq.com>
2021-06-10 21:46:33 +0800

    20210609-力扣刷题

---
+++ src/likou_dp_1.java
@@ -0,0 +1,110 @@
+/**
+ * @Auther: chendongtao
+ * @Date: 2021/6/10 16:32
+ * @Description:
+ */
+public class likou_dp_1 {
+    public static void main(String[] args) {
+        System.out.println(longestPalindrome1("cbbd"));
+    }
+
+    /**
+     * 5. 最长回文子串
+     *
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome(String s) {
+        if (s.length() < 2) {
+            return s;
+        }
+        int len = s.length();
+        int maxLen = 1;
+        int begin = 0;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (j - i + 1 > maxLen && validPalindromic(chars, i, j)) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+    private static boolean validPalindromic(char[] chars, int i, int j) {
+        while (i < j) {
+            if (chars[i] != chars[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    //动态规划
+    public static String longestPalindrome1(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        int begin = 0;
+        char[] chars = s.toCharArray();
+        //dp数组
+        boolean[][] dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            dp[i][i] = true;
+        }
+        for (int L = 2; L <= len; L++) {
+            for (int i = 0; i < len; i++) {
+                int j = L + i - 1;
+                if (j >= len) {
+                    break;
+                }
+                if (chars[i] != chars[j]) {
+                    dp[i][j] = false;
+                } else {
+                    if (j - i < 3) {
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                if (dp[i][j] && j - i + 1 > maxLen) {
+                    maxLen = j - i + 1;
+                    begin = i;
+                }
+            }
+        }
+        return s.substring(begin, begin + maxLen);
+    }
+
+    //中心扩展算法
+    public static String longestPalindrome2(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+}
